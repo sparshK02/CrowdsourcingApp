dd1863ce811161cdff73246a9253d16f8bb8d629
Sparsh Kustagishettar <sparsh1910@gmail.com>
2022-06-22 15:57:47 -0400

    Home page completed and all links functional except log in

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
+  root "static_pages#home"
+  get '/instructions', to: 'static_pages#instructions'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "static_pages#home"
+
 end
