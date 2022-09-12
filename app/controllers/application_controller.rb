63ffe735c2ffe3813b1f95024cf806f329018481
entslscheia <yugu772@yahoo.com>
2022-09-12 10:24:57 -0400

    Data Annotated

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,7 @@ class ApplicationController < ActionController::Base
             annotation.save
             redirect_to makes_sense_path(annotation)
             return
-          elsif (annotation.assigned == true) && (annotation.finished == false) && (Time.current - annotation.updated_at > 12*60*60)
+          elsif (annotation.assigned == true) && (annotation.finished == false) && (Time.current - annotation.updated_at > 1*60*60)
             annotation.assigned = 0
             annotation.save
             annotation.assigned = 1
